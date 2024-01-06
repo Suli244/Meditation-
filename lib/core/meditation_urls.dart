b5abf961a5fc39b977431fd5f74ee0bc623afb34
Ravshan <kasimovravshan03@gmail.com>
2024-01-06 16:50:46 +0600

    ready for release

--- lib/core/meditation_urls.dart
+++ lib/core/meditation_urls.dart
@@ -1,6 +1,6 @@
 class MeditationUrls {
-  static const String apphudApiKey = 'app_Syk68hpZF7W69zdEPxHPdU26i3H44m';
-  static const String privacy = 'https://pub.dev/';
-  static const String use = 'https://pub.dev/';
-  static const String support = 'https://pub.dev/';
+  static const String apphudApiKey = 'app_cTvtL6LxitdZLvQdpCQTv7PCPAXd4B';
+  static const String privacy = 'https://docs.google.com/document/d/1c41zy6rumJlo0FpMlQgNeP_wI0oJzI0J9RAoaeycNn8/edit?usp=sharing';
+  static const String use = 'https://docs.google.com/document/d/1h42FdFZ_NOmp1Cy4SmitA-9rwOxxYcZckgCh58jpAwY/edit?usp=sharing';
+  static const String support = 'https://sites.google.com/view/xasolutionsgmbh/support-form';
 }
